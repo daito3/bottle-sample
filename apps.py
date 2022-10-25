8e2b09a088e7b4f579573bcf678184a75ba79d93
大東 <daitou@oohigashis-MacBook-Air.local>
2022-10-25 10:29:28 +0900

    first commit

---
+++ apps.py
@@ -0,0 +1,16 @@
+import bottle
+#各パスルーティング
+import routes
+import routes_form
+import routes_list
+import routes_login
+from utils.session import Session
+ 
+app = routes.app
+app_sess = routes.app_sess
+
+if __name__ == '__main__':
+    # this setting is running for development.
+    bottle.run(app=app_sess, host='0.0.0.0',port=8888, reloader=True, debug=True)
+else:
+    application = app_sess
